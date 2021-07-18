596402115ebf7401b52f64c40f47d5dab36c9051
keenan <keenanburke@outlook.com>
2021-07-17 23:27:23 -0400

    smooth rotate mark 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,71 @@
+#include <Servo.h>
+#include <math.h>
 #include "Arduino.h"
-#include <arm.h>
 
-void setup()
+#define LKY62_MAX_ROT 180
+#define LKY62_MIN_ROT 0
+
+float getAng(float x, float endAng, float P, float K)
 {
+  float ang = P / (1.0 + exp(K*x)) + endAng;
+
+  return ang;
+}
+
+Servo servo;
+float servoAng;
+
+void setup() {
   Serial.begin(9600);
-  initServos();
+
+  pinMode(8, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  
+  servo.attach(8);
+  servo.write(0);
 }
 
-SERVO_DIR servoInputDir;
-int servoAngle;
-
-void indent(int numSpaces){
-  for(int spacesPrinted = 0; spacesPrinted < numSpaces; spacesPrinted++) {
-    Serial.print("    ");
-  }
-}
-
-void loop()
+void setServoTo(float* servoAng, float targetAng)
 {
-  for(int servoIdx = 0; servoIdx < NUM_ARM_MEMBERS; servoIdx++) {
-    servoInputDir = getServoInputDir(servoIdx);
+  float startAng = *servoAng;
+  
+  float P = startAng - targetAng;
+  float K = pow(fabs(P), -0.65);
 
-    servoAngle = computeServoAngle(servoIdx, servoInputDir);
-    //indent(servoIdx);
-    //Serial.println(servoAngle);
+  float startX = log(1.0 / (fabs(1-180*P) ) ) / K; 
+  float lastX = -startX;
+  float x = startX;
 
-    writeServoAngle(servoIdx, servoAngle);
+  char debug[50];
+  sprintf(debug, "startX %d, lastX %d", startX, lastX);
+  Serial.println(debug);
 
-    delay(5);
+  for(; x < lastX; x += 0.03)
+  {
+    *servoAng = getAng(x, targetAng, P, K);
+    servo.write((*servoAng));
+    //delay(10);
   }
+}
+
+void loop() {
+
+  if(digitalRead(2))
+  {
+    Serial.println("2");
+    servoAng = 0;
+    servo.write(servoAng);
+  }
+  else if(digitalRead(3))
+  {
+    Serial.println("3");
+    setServoTo(&servoAng, 180);
+    servo.write(servoAng);
+  }
+  
+
+  
+  
+  
 }
